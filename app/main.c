57e7a4be1cac71f4f1b73d502eee90fd062c6520
Sami <b.sami.code@gmail.com>
2024-10-05 23:59:29 +0200

    Exit builtin

--- app/main.c
+++ app/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
   while(1){
@@ -9,6 +10,8 @@ int main() {
     char input[100];
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
+    if (!strcmp(input, "exit 0"))
+      exit(0);
     printf("%s: command not found\n", input);
   }
   return 0;
